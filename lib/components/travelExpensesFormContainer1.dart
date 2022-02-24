2eb354f2ec2111fccd3b097760ed6b0df6fe6763
unknown <wildser@gmail.com>
2022-02-23 22:25:59 -0500

    viaje asociado added

--- lib/components/travelExpensesFormContainer1.dart
+++ lib/components/travelExpensesFormContainer1.dart
@@ -10,6 +10,7 @@ class FormContainer1 extends StatefulWidget {
 var value;
 var dateValueIni;
 var dateValueEnd;
+var _items3;
 
 class _FormContainer1 extends State<FormContainer1> {
   DateTime currentDate = DateTime.now();
@@ -67,17 +68,15 @@ class _FormContainer1 extends State<FormContainer1> {
                     icon: Icon(Icons.public),
                     labelText: 'Viaje asociado a:',
                     items: _items2,
-                    onChanged: (val) => {value = val},
-                  
+                    onChanged: (val) => {value = val, func()},
                   ),
                   SelectFormField(
                     style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
                     initialValue: 'negocio',
                     icon: Icon(Icons.public),
                     labelText: 'Viaje asociado a:',
-                    items: _items2,
+                    items: _items3,
                     onChanged: (val) => {value = val},
-                  
                   )
                 ])),
             Container(
@@ -98,7 +97,6 @@ class _FormContainer1 extends State<FormContainer1> {
                       hintStyle:
                           TextStyle(fontSize: 14, fontWeight: FontWeight.bold)),
                 )),
-                
             ElevatedButton(
               style: ButtonStyle(
                   fixedSize: MaterialStateProperty.all(Size.fromWidth(10))),
@@ -109,7 +107,7 @@ class _FormContainer1 extends State<FormContainer1> {
               ),
             ),
             ElevatedButton(
-              onPressed: () =>func(),//_selectDate2(context),
+              onPressed: () => func(), //_selectDate2(context),
               child: Text(
                 'Seleccione Fecha Fin',
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
@@ -142,9 +140,11 @@ class _FormContainer1 extends State<FormContainer1> {
       'textStyle': TextStyle(color: Colors.red),
     },
   ];
-void func()async{
-  Future<List> _futureOfList = viajeAsociadoFetchData();
-  List list = await _futureOfList ;
-  print(list);
-}
+  void func() async {
+    Future<List> _futureOfList = viajeAsociadoFetchData();
+    List list = await _futureOfList;
+    setState(() {
+      _items3 = list;
+    });
+  }
 }
--- lib/utilities/fetch.dart
+++ lib/utilities/fetch.dart
@@ -1,48 +1,46 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:msm_mobile_app/models/login_response.dart';
 import 'package:msm_mobile_app/models/viajeAsociadoResponse.dart';
 
-Future <User?>loginFetchData(password, user) async {
-  const url= 'https://www.mv-tel.com/MSM_Viaticos_API_TEST/api/Usuarios/LoginUserPassword';
-  
+Future<User?> loginFetchData(password, user) async {
+  const url =
+      'https://www.mv-tel.com/MSM_Viaticos_API_TEST/api/Usuarios/LoginUserPassword';
+
   final response = await http.post(Uri.parse(url),
-  headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-  body:jsonEncode(<String, String>{
-    'username': user,
-    'password': password
-  }));
- if (response.statusCode == 200){
-    final  body = jsonDecode(response.body);
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body:
+          jsonEncode(<String, String>{'username': user, 'password': password}));
+  if (response.statusCode == 200) {
+    final body = jsonDecode(response.body);
     final responseData = User.fromJson(body);
-    return responseData;}
-  else{
+    return responseData;
+  } else {
     final responseData = null;
     return responseData;
   }
 }
 
-Future <List<Map<String, dynamic>>>viajeAsociadoFetchData() async {
-  const url= 'https://www.mv-tel.com/MSM_Viaticos_API_Test/api/CuentaContable/ObtenerCuentasContablesPorViajeAsociado/1';
-  
-  final response = await http.get(Uri.parse(url),
-  
+Future<List<Map<String, dynamic>>> viajeAsociadoFetchData() async {
+  const url =
+      'https://www.mv-tel.com/MSM_Viaticos_API_Test/api/CuentaContable/ObtenerCuentasContablesPorViajeAsociado/1';
+
+  final response = await http.get(
+    Uri.parse(url),
   );
- if (response.statusCode == 200){
-    final  body = jsonDecode(response.body);
-    final responseData =  ViajeAsociadoList.fromJson(body);
-    List<Map<String, dynamic>>myList=[];
-    for (var i in responseData.viajes){
-      myList.add({'value': '${i.codigo}', 'label':'${i.codigo}' });
+  if (response.statusCode == 200) {
+    final body = jsonDecode(response.body);
+    final responseData = ViajeAsociadoList.fromJson(body);
+    List<Map<String, dynamic>> myList = [];
+    for (var i in responseData.viajes) {
+      myList.add({'value': '${i.codigo}', 'label': '${i.codigo}'});
     }
-    return myList;}
-  else{
+    return myList;
+  } else {
     final responseData = null;
     print(response.statusCode);
     return responseData;
   }
 }
-
