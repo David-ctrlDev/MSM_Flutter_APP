ea31f81fd898a4a6cd0780c3b61b81b2b6fd7d53
Wilder David Serna Montoya <wildser@gmail.com>
2022-03-02 17:42:17 -0500

    Add Staggeredgrid

--- lib/components/travelExpensesFormContainer2.dart
+++ lib/components/travelExpensesFormContainer2.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:intl/intl.dart';
 import 'package:msm_mobile_app/components/travelExpensesFormContainer1.dart';
 import 'package:msm_mobile_app/utilities/fetch.dart';
@@ -35,6 +36,7 @@ class _FormContainer2 extends State<FormContainer2> {
 
   Widget build(BuildContext context) {
     return (Container(
+      
       child: Column(
         children: [
           ElevatedButton(
@@ -55,87 +57,192 @@ Future<void> _showMyDialogMain(context) async {
     barrierDismissible: false, // user must tap button!
     builder: (BuildContext context,) {
       return AlertDialog(
+        backgroundColor: Colors.white,
+         insetPadding: EdgeInsets.all(10),
+         contentPadding: EdgeInsets.all(10),
         title: const Text('Destinos',style: TextStyle(fontWeight: FontWeight.bold),),
         content: Container(
-          height: MediaQuery.of(context).size.height/2,
+          height: MediaQuery.of(context).size.height/1.35,
           width: MediaQuery.of(context).size.width ,
           child:
-         GridView(
-          gridDelegate:SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,childAspectRatio: 1/1.2),
+         StaggeredGrid.count(
+            crossAxisCount: 4,
+            mainAxisSpacing: 4,
+            crossAxisSpacing: 4,
+         
           children: <Widget>[
-        Card(
-          child: Container(
-            height: 250,
-            decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(
-            image: NetworkImage("https://images.unsplash.com/photo-1585871746932-e133d3fedf4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=770&q=80"),
-            fit: BoxFit.fitHeight,
-            alignment: Alignment.topCenter,
-          )),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-            ListTile(
-              title: Text("País", style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),
-            ),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-             children: [
-               ElevatedButton(
-                 child: const Text('Buscar'),
-                 onPressed: () {_showMyDialog(context);},
-               )]),
-          ],),)),
-      Card(
-          child: Container(
-            height: 250,
-            decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            image: DecorationImage(
-            image: NetworkImage("https://images.unsplash.com/photo-1568668392383-58c369615742?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=719&q=80"),
-            fit: BoxFit.fitWidth,
-            alignment: Alignment.topCenter,
-          )),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-            ListTile(
-              title: Text("Ciudad", style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),
-            ),
-            ButtonBar(
-              alignment: MainAxisAlignment.center,
-             children: [
-               ElevatedButton(
-                 child: const Text('Buscar'),
-                 onPressed: () {_showMyDialog2(context);},
-               )]),
-          ],),)),
-         Card(
-           child: ElevatedButton(
-                 style: ButtonStyle(
-              fixedSize: MaterialStateProperty.all(Size.fromWidth(220))),
-                 onPressed: () => _selectDate(context),
-                 child: Text(
-            selectDate1TextDestination,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
-                 ),
-               ),
-         ),
-      Card(
-        child: ElevatedButton(
-          style: ButtonStyle(
-              fixedSize: MaterialStateProperty.all(Size.fromWidth(220))),
-          onPressed: () => _selectDate2(context),
-          child: Text(
-            selectDate2TextDestination,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
-          ),
-        
+        StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 2,
+          child: Card(
+            shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(20), ),
+            child: Container(
+              decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+              image: NetworkImage("https://images.unsplash.com/photo-1585871746932-e133d3fedf4d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=770&q=80"),
+              fit: BoxFit.cover,
+              alignment: Alignment.topCenter,
+            )),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+              ListTile(
+                title: Text("País", style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),
+              ),
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
+               children: [
+                 ElevatedButton(
+                   child: const Text('Buscar'),
+                   onPressed: () {_showMyDialog(context);},
+                 )]),
+            ],),)),
         ),
-      )]
-      
-  )),
+        StaggeredGridTile.count(
+             crossAxisCellCount: 2,
+          mainAxisCellCount: 2,
+        child: Card(
+          
+          shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(20), ),
+            child: Container(
+              decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+              image: NetworkImage("https://images.unsplash.com/photo-1568668392383-58c369615742?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=719&q=80"),
+              fit: BoxFit.fitWidth,
+              alignment: Alignment.topCenter,
+            )),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+              ListTile(
+                title: Text("Ciudad", style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,fontSize: 20),),
+              ),
+              ButtonBar(
+                alignment: MainAxisAlignment.center,
+               children: [
+                 ElevatedButton(
+                   child: const Text('Buscar'),
+                   onPressed: () {_showMyDialog2(context);},
+                 )]),
+            ],),)),
+      ),
+       StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 0.5,
+         child: ElevatedButton(
+                       
+                       onPressed: () => _selectDate(context),
+                       child: Text(
+                  selectDate1TextDestination,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+                       ),
+                     ),
+       ),
+        StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 0.5,
+          child: ElevatedButton(
+            
+            onPressed: () => _selectDate2(context),
+            child: Text(
+              selectDate2TextDestination,
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),
+            )),
+        ),
+         StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 1,
+           child:TextFormField(
+                        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 10),
+            labelText: 'Dist Inpu',
+            icon: Icon(Icons.text_rotation_none_rounded,),
+                                             
+         ))),
+         StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 1,
+           child:TextFormField(
+                        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 10),
+            labelText: 'Centro de Costos',
+            icon: Icon(Icons.text_rotation_none_rounded,),
+                                             
+         ))),
+         StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 1,
+           child:TextFormField(
+                        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 10),
+            labelText: 'Centro Gestor',
+            icon: Icon(Icons.text_rotation_none_rounded,),
+                                             
+         ))),
+         StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 1,
+           child:TextFormField(
+                        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 10),
+            labelText: 'Orden Interna',
+            icon: Icon(Icons.text_rotation_none_rounded,),
+                                             
+         ))),
+         StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 1,
+           child:TextFormField(
+                        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 10),
+            labelText: 'Pospre',
+            icon: Icon(Icons.text_rotation_none_rounded,),
+                                             
+         ))),
+         StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 1,
+           child:TextFormField(
+                        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 10),
+            labelText: 'Grafo',
+            icon: Icon(Icons.text_rotation_none_rounded,),
+                                             
+         ))),
+         StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 1,
+           child:TextFormField(
+                        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 10),
+            labelText: 'Operación',
+            icon: Icon(Icons.text_rotation_none_rounded,),
+                                             
+         ))),
+         StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 1,
+           child:TextFormField(
+                        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 10),
+            labelText: 'Elemento PeP',
+            icon: Icon(Icons.text_rotation_none_rounded,),
+                                             
+         ))),
+         StaggeredGridTile.count(
+          crossAxisCellCount: 2,
+          mainAxisCellCount: 1,
+           child:TextFormField(
+                        decoration: const InputDecoration(
+            contentPadding: EdgeInsets.symmetric(vertical: 10),
+            labelText: 'Fondo',
+            icon: Icon(Icons.text_rotation_none_rounded,),
+                                             
+         ))),
+         
+         ],)),
    actions: <Widget>[
           TextButton(
             child: const Text('Agregar'),
