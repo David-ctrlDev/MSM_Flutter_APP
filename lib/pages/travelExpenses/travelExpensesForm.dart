bf4f6fe687f0e2a7096b920fae1581c67d788bbf
Wilder David Serna Montoya <wildser@gmail.com>
2022-03-09 16:16:33 -0500

    Fixed Steps Bug

--- lib/pages/travelExpenses/travelExpensesForm.dart
+++ lib/pages/travelExpenses/travelExpensesForm.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:msm_mobile_app/components/alertDialog.dart';
+import 'package:msm_mobile_app/components/cameraPicture.dart';
 import 'package:msm_mobile_app/components/draggableActionButton.dart';
 import 'package:msm_mobile_app/components/travelExpensesFormContainer1.dart';
 import 'package:msm_mobile_app/components/travelExpensesFormContainer2.dart';
@@ -24,11 +25,11 @@ class _TravelExpensesFormState extends State<TravelExpensesForm> {
     this.data,
   );
 
-  void showAlert(BuildContext context) {}
+
 
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    Future.delayed(Duration.zero, () => showAlert(context));
+  
 
     return (Scaffold(
         floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
@@ -66,7 +67,7 @@ class _TravelExpensesFormState extends State<TravelExpensesForm> {
                                 style: TextStyle(
                                   color: Colors.white,
                                   fontWeight: FontWeight.bold,
-                                  fontSize: 25,
+                                  fontSize: 18,
                                 ),
                                 textAlign: TextAlign.left,
                               )
@@ -77,7 +78,7 @@ class _TravelExpensesFormState extends State<TravelExpensesForm> {
                             style: TextStyle(
                               color: Colors.white,
                               fontWeight: FontWeight.bold,
-                              fontSize: 18,
+                              fontSize: 14,
                             ),
                           )
                         ],
@@ -113,27 +114,27 @@ class _TravelExpensesFormState extends State<TravelExpensesForm> {
                              case 0:{
                              
                                 if(associatedValue == null || associatedValue== ""){
-                                  alertDialog(context,);
+                                  alertDialog(context, "Destino");
                                   currentStep = -1;
                                 }
                                 else if(accountValue == null || accountValue ==""){
-                                  alertDialog(context,);
+                                  alertDialog(context,"Viaje Asociado");
                                   currentStep = -1;
                                 }
                                 else if(travelObject.text ==""){
-                                  alertDialog(context,);
+                                  alertDialog(context,"Objeto del Viaje");
                                   currentStep = -1;
                                 }
                                 else if(sapNumber.text ==""){
-                                  alertDialog(context,);
+                                  alertDialog(context,"Número Sap");
                                   currentStep = -1;
                                 }
                                 else if(dateValueIni == null || dateValueIni ==""){
-                                  alertDialog(context,);
+                                  alertDialog(context,"Viaje Asociado");
                                   currentStep = -1;
                                 }
                                 else if(dateValueEnd == null || dateValueEnd ==""){
-                                  alertDialog(context,);
+                                  alertDialog(context,"Viaje Asociado");
                                   currentStep = -1;
                                 }
                              } 
@@ -181,7 +182,7 @@ List<Step> getSteps() => [
       Step(
           isActive: currentStep >= 4,
           title: Text("Observaciones"),
-          content: Container()),
+          content: ImagePickerWidget()),
     ];
 
 class SimpleClipper2 extends CustomClipper<Path> {
